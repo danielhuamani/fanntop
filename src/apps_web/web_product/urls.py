22b8171857aa322d9e8c7b283fda494495aa7cb6
danielhuamani <danielhuamani15@gmail.com>
2018-01-27 18:58:23 -0500

    categorias productos

--- src/apps_web/web_product/urls.py
+++ src/apps_web/web_product/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from .views import influencer_products
+from .views import influencer_products, category_products, category_child_products
 
 urlpatterns = [
-    url(r"^influenciador/(?P<pk>\d+)/$", influencer_products, name="influencer_products"),
+    url(r"^influenciador/(?P<slug>[\w-]+)/$", influencer_products, name="influencer_products"),
+    url(r"^categoria/(?P<slug>[\w-]+)/$", category_products, name="category_products"),
+    url(r"^categoria/(?P<slug>[\w-]+)/(?P<slug_child>[\w-]+)/$", category_child_products, name="category_child_products"),
+
 ]
--- src/apps_web/web_product/views.py
+++ src/apps_web/web_product/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from apps_base.influencer.models import Influencer
+from apps_base.category.models import Category
 
-
-def influencer_products(request, pk):
-    influencer = get_object_or_404(Influencer, pk=pk)
+def influencer_products(request, slug):
+    influencer = get_object_or_404(Influencer, slug=slug)
     ctx = {
         'influencer': influencer
     }
     return render(request, 'product/influencer_list_products.html', ctx)
+
+def category_products(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    ctx = {
+        'category': category
+    }
+    return render(request, 'product/category_list_products.html', ctx)
+
+
+def category_child_products(request, slug, slug_child):
+    category = get_object_or_404(Category, category__slug=slug, slug=slug_child)
+    ctx = {
+        'category': category
+    }
+    return render(request, 'product/category_list_products.html', ctx)
