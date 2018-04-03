64181987f29f9211f15dd460ad46ab379244a027
danielhuamani <danielhuamani15@gmail.com>
2018-04-03 12:50:37 -0500

    mejorando favoritos

--- src/resources/src/components/category/productListFavorites.vue
+++ src/resources/src/components/category/productListFavorites.vue
@@ -8,20 +8,27 @@
                 <h3>Mis Favoritos</h3>
             </div>
         </div>
-        <div class="row">
-          <div class="col-md-4"  v-for='product in products.results' >
-            <a :href='"/producto/" + product.slug' class="box item-result">
-              <div class="image"> <img :src="product.product_variant.product_image.image"/></div>
-              <div class="detail">
-                <h3 class="title">{{product.name}}</h3>
-                <!-- <h4 class="subtitle">subtitle</h4> -->
-                <div class="price">
-                  <span class="offer">S/. {{product.product_variant.price}}</span>
-                  <!-- <span class="normal tachado">S/. 80.15 PEN</span> -->
-                </div>
-              </div>
-            </a>
-          </div>
+        <div class="row" v-if='products.results.length > 0'>
+            <div class="col-md-4"   v-for='product in products.results' >
+                <a :href='"/producto/" + product.slug' class="box item-result">
+                  <div class="image"> <img :src="product.product_variant.product_image.image"/></div>
+                  <div class="detail">
+                    <h3 class="title">{{product.name}}</h3>
+                    <!-- <h4 class="subtitle">subtitle</h4> -->
+                    <div class="price">
+                      <span class="offer">S/. {{product.product_variant.price}}</span>
+                      <!-- <span class="normal tachado">S/. 80.15 PEN</span> -->
+                    </div>
+                  </div>
+                </a>
+           </div>
+        </div>
+        <div class="row row-no-product"  v-else >
+
+            <div class="col-md-12">
+                No tiene Productos Favoritos
+            </div>
+
         </div>
       </div>
     </div>
@@ -40,3 +47,9 @@
     }
   }
 </script>
+<style>
+.row-no-product{
+    margin-top: 40px;
+    height: 200px;
+}
+</style>
